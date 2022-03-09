fe7d3718941981778fe77fee7070067c0a13d576
Dipesh Kakadiya <dipesh.kakadiya111@gmail.com>
2022-03-09 18:42:29 +0530

    Fix following bugs with Country Card Block - Code formatting - Fixed issue with Toolbar block controls to edit selection - Preview functionality we can handle with countryCode

--- src/save.js
+++ src/save.js
@@ -1,8 +1,20 @@
+/**
+ * WordPress dependencies
+ */
+import { useBlockProps } from '@wordpress/block-editor';
+
+/**
+ * Internal dependencies
+ */
 import Preview from './preview';
 
 export default function Save( { attributes } ) {
+	const { align } = attributes;
+
+	const wrapperClasses = align !== null ? 'align' + align : '';
+
 	return (
-		<div>
+		<div { ...useBlockProps.save( { className: wrapperClasses } ) }>
 			<Preview { ...attributes } />
 		</div>
 	);
